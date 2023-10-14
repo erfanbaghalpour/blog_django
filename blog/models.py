afef7538ad40b97cc84bcc2f8d8e1026b3161255
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-10-14 22:12:13 +0330

    first commit

---
+++ blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'Df', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+        REJECTED = 'RJ', 'Rejected'
+
+    # relations
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
+    # data fields
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    slug = models.SlugField(max_length=250)
+    # date
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # choice fields
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"])
+        ]
+
+    def __str__(self):
+        return self.title
