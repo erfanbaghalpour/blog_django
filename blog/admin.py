4b5fec8414aedecd49d47eccb3cd0a1682442ecf
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-10-15 22:58:37 +0330

    Model admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Post)
 
-
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish', 'status']
+    ordering = ['title', 'publish']
+    list_filter = ['status', 'author', 'publish']
+    search_fields = ['title', 'description']
+    date_hierarchy = 'publish'
+    prepopulated_fields = {"slug": ['title']}
