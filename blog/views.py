49f589e3ade446894df1fba22f3840d4424fd828
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-10-15 23:44:02 +0330

    URL and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("index")
+
+
+def post_list(request):
+    return HttpResponse("post list")
+
+
+def post_detail(request, id):
+    return HttpResponse("post_detail:", id)
