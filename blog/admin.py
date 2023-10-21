eeaeb54b595d310926df1ef533064671ec0d4f64
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-10-21 18:34:17 +0330

    Persian Date 2

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import *
+from django_jalali.admin.filters import JDateFieldListFilter
+import django_jalali.admin as jadmin
 
 admin.sites.AdminSite.site_header = "پنل مدیریت جنگو"
 admin.sites.AdminSite.site_title = "پنل "
 admin.sites.AdminSite.index_title = "پنل مدیریت "
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'publish', 'status']
     ordering = ['title', 'publish']
-    list_filter = ['status', 'author', 'publish']
+    list_filter = ['status', 'author', ('publish', JDateFieldListFilter)]
     search_fields = ['title', 'description']
     date_hierarchy = 'publish'
     prepopulated_fields = {"slug": ['title']}
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django_jalali.db import models as jmodels
 
 
 # Managers
@@ -22,13 +23,14 @@ class Post(models.Model):
     description = models.TextField(verbose_name="توضیحات")
     slug = models.SlugField(max_length=250, verbose_name="اسلاگ")
     # date
-    publish = models.DateTimeField(default=timezone.now, verbose_name="تاریخ پست")
-    created = models.DateTimeField(auto_now_add=True, verbose_name="ساخته شده در")
-    updated = models.DateTimeField(auto_now=True, verbose_name="به روز رسانی شده در")
+    publish = jmodels.jDateTimeField(default=timezone.now, verbose_name="تاریخ پست")
+    created = jmodels.jDateTimeField(auto_now_add=True, verbose_name="ساخته شده در")
+    updated = jmodels.jDateTimeField(auto_now=True, verbose_name="به روز رسانی شده در")
     # choice fields
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT, verbose_name="تصویر")
 
-    objects = models.Manager()
+    # objects = models.Manager()
+    objects = jmodels.jManager()
     published = PublishedManager()
 
     class Meta:
