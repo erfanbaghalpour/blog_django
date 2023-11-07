38e950493b0c2305adffb5577a24c9f00206842a
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-11-07 19:38:09 +0330

    Postgre

--- config/settings.py
+++ config/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog_admin',
+        'PASSWORD': 'f!!@@@7&&',
+        'PORT': '5432',
     }
 }
 
