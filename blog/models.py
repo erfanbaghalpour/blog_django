0314354d10fd8736dcba621d1d37f78b8d096419
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2024-05-23 18:45:33 +0330

    django-resized

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django_jalali.db import models as jmodels
 from django.urls import reverse
+from django_resized import ResizedImageField
 
 
 # Managers
@@ -87,7 +88,7 @@ class Comment(models.Model):
 
 class Image(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images", verbose_name="تصویر")
-    image_file = models.ImageField(upload_to="post_images/")
+    image_file = ResizedImageField(upload_to="post_images/", size=[500, 500], quality=95, crop=['middle', 'center'])
     title = models.CharField(max_length=250, verbose_name="عنوان", null=True, blank=True)
     description = models.TextField(verbose_name="توضیحات", null=True, blank=True)
     created = jmodels.jDateTimeField(auto_now_add=True, verbose_name="ساخته شده در")
@@ -100,5 +101,5 @@ class Image(models.Model):
         verbose_name = "تصویر"
         verbose_name_plural = "تصاویر"
 
-    def __str__(self):
-        return self.title
+    # def __str__(self):
+    #     return self.title
